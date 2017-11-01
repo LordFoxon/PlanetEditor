d2af907ec443f3bf96b4633ad5e154416b44c41c
cyr471 <cyr471@my.utsa.edu>
2017-10-31 19:27:56 -0500

    Started planetName
    Will have to change error reporting, but the basic structure is there.
    Also, have to fix bug with planetDiameter.

--- CS4773Assignment3/src/planet/detail/PlanetController.java
+++ CS4773Assignment3/src/planet/detail/PlanetController.java
@@ -4,6 +4,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -51,4 +54,46 @@ public class PlanetController {
     @FXML
     void savePlanet(ActionEvent event) {
     }
+    
+    @FXML
+    void setPlanetName(ActionEvent event) {
+    	//if required to update as typed, have to implement different solution
+//    	planetName.getDocument().addDocumentListener(new DocumentListener() { 
+//    	@Override
+//         public void insertUpdate(DocumentEvent de) {
+//             fancyPlanetName.setText(planetName.getText());
+//         }
+//
+//         @Override
+//         public void removeUpdate(DocumentEvent de) {
+//        	 fancyPlanetName.setText(planetName.getText());
+//         }
+//
+//         @Override
+//         public void changedUpdate(DocumentEvent de) {
+//         //Plain text components don't fire these events.
+//         }
+//     });
+
+    	//set fancy name to be updated
+    	fancyPlanetName.setText(planetName.getText());
+    	//TODO will have to change all invalid fields to alert or set boolean that says that validating failed
+    	//check if planet name is within valid range
+    	//TODO he suggested the validator should be delegated
+    	//TODO builder to make planet object
+    	if (fancyPlanetName.getText().length() < 1 || fancyPlanetName.getText().length() > 255)
+    		System.out.println("ERROR: planet name must be between 1 and 255 characters long.");
+    	//check for illegal characters
+    	if (!fancyPlanetName.getText().matches("[-a-zA-Z0-9. ]+") )
+    		System.out.println("ERROR: planet name can only contain alphanumeric and/or punctation characters (\".\", \"-\", or \" \").");
+    	//Check for empty string - I think the above two already cover this
+    	//TODO initialize planetName to empty i.e ""
+    	if (planetName.getText().equals(""))
+    		System.out.println("ERROR: planet name field was not set.");
+    }
+    
+    void setPlanetDiameter(ActionEvent event) {
+    	System.out.println(planetDiameterKM.getText());
+    }
+    
 }
