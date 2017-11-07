7bd18d130b8ca5d65157a357ebf52214336325a2
cyr471 <cyr471@my.utsa.edu>
2017-11-06 18:43:39 -0600

    Added First Test Case
    Not exactly sure how to test since it is a GUI.

---
+++ CS4773Assignment3/src/test/PlanetTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import planet.detail.PlanetController;
+
+public class PlanetTest {
+
+	@Test
+	public void test(){
+	}
+
+}
